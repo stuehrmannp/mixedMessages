aa4a16c24a38bdccdf8654bed94c24b4d45a75c2
Sthphil <sthphilsers.noreply.github.com>
2021-08-06 11:02:52 +0200

    Randomizer to randomize the messages coming from the arrays filled with words

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 const motivationalMessages = ['You can do it!', 'There is another way!', 'You got big things ahead of you!','You are going to be great!', 'You are going to make it!'];
+const funnyWords = ['Bugha', 'lol', 'hippie', 'Lard', 'dude', 'punk', 'scrub', 'dad', 'oldie'];
+const names = ['Clown', 'Personal chef', 'Boss', 'Hawk', 'leader', 'christian', 'priest'];
+
+let randomizer = Math.floor(Math.random() * motivationalMessages.length);
+
+ let message = `Hey there ${names[randomizer]}, just wanted to tell you: ${motivationalMessages[randomizer]}! Keep it up, ${funnyWords[randomizer]}`;
+
